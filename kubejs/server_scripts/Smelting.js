c5a29cdd79cbd7fb61332a663a41f40aca6be590
XZLTO <66406449+XZLTO@users.noreply.github.com>
2026-02-28 21:20:40 +0700

    Test

--- kubejs/server_scripts/Smelting.js
+++ kubejs/server_scripts/Smelting.js
@@ -6,25 +6,25 @@ BlockEvents.rightClicked((event) => {
     lastUsedBlock[event.player.name] = event.block;
 })
 
-Recepie
 
-ItemEvents.smelted((event) => {
-    let player = event.player;
-    let item = event.item;
+
+// ItemEvents.smelted((event) => {
+//     let player = event.player;
+//     let item = event.item;
     
-    if (lastUsedBlock[player.name] != null && lastUsedBlock[player.name].id == 'minecraft:furnace' && item.hasTag("forge:ingots")) {
-        let block = lastUsedBlock[player.name];
-        let count = item.getCount();
+//     if (lastUsedBlock[player.name] != null && lastUsedBlock[player.name].id == 'minecraft:furnace' && item.hasTag("forge:ingots")) {
+//         let block = lastUsedBlock[player.name];
+//         let count = item.getCount();
 
-        let random = Math.random()
-        player.tell(`random:${random} round:${Math.round(count * random)}`)
-        item.setCount(Math.round(count * random));
+//         let random = Math.random()
+//         player.tell(`random:${random} round:${Math.round(count * random)}`)
+//         item.setCount(Math.round(count * random));
 
-        let coal = block.createEntity("item")
-        coal.item = Item.of("coal")
-        coal.item.count = (item.getCount() - count)/2
-        coal.x = block.x;
-        coal.y = block.y;
-        coal.z = block.z;
-    }
-})
+//         let coal = block.createEntity("item")
+//         coal.item = Item.of("coal")
+//         coal.item.count = (item.getCount() - count)/2
+//         coal.x = block.x;
+//         coal.y = block.y;
+//         coal.z = block.z;
+//     }
+// })
