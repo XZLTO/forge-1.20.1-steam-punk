63ac76a90b581c36e66023998e04e6bf12991a46
XZLTO <66406449+XZLTO@users.noreply.github.com>
2026-03-01 20:30:37 +0700

    Update

--- kubejs/server_scripts/Smelting.js
+++ kubejs/server_scripts/Smelting.js
@@ -1,30 +1,30 @@
+const SMELT_DATA = [
+  {
+    output: 'minecraft:iron_ingot',
+    nugget: 'minecraft:iron_nugget',
+    raw   : 'minecraft:raw_iron'
+  },
+  {
+    output: 'minecraft:copper_ingot',
+    nugget: 'create:copper_nugget',
+    raw   : 'minecraft:raw_copper'
+  },
+  {
+    output: 'minecraft:gold_ingot',
+    nugget: 'minecraft:gold_nugget',
+    raw   : 'minecraft:raw_gold'
+  },
+  {
+    output: 'create:zinc_ingot',
+    nugget: 'create:zinc_nugget',
+    raw   : 'create:raw_zinc'
+  }
+]
 
-/** @type {Object.<Internal.Component, Internal.BlockContainerJS>} */
-const lastUsedBlock = {}
 
-BlockEvents.rightClicked((event) => {
-    lastUsedBlock[event.player.name] = event.block;
+ServerEvents.recipes(event => {
+  for (const entry of SMELT_DATA) {
+    event.remove([{ type: 'minecraft:smelting', output:entry.output}])
+    event.smelting(`3x ${entry.nugget}`, entry.raw)
+  }
 })
-
-
-
-// ItemEvents.smelted((event) => {
-//     let player = event.player;
-//     let item = event.item;
-    
-//     if (lastUsedBlock[player.name] != null && lastUsedBlock[player.name].id == 'minecraft:furnace' && item.hasTag("forge:ingots")) {
-//         let block = lastUsedBlock[player.name];
-//         let count = item.getCount();
-
-//         let random = Math.random()
-//         player.tell(`random:${random} round:${Math.round(count * random)}`)
-//         item.setCount(Math.round(count * random));
-
-//         let coal = block.createEntity("item")
-//         coal.item = Item.of("coal")
-//         coal.item.count = (item.getCount() - count)/2
-//         coal.x = block.x;
-//         coal.y = block.y;
-//         coal.z = block.z;
-//     }
-// })
